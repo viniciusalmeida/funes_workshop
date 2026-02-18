4978e822773a86f307c94489c60dfeb7d1d89bf6
Vinícius Almeida da Silva <vinialm88@gmail.com>
2026-02-18 15:46:46 -0300

    fix controller initialization issue

--- app/javascript/controllers/date_field_state_controller.js
+++ app/javascript/controllers/date_field_state_controller.js
@@ -1,6 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    if (this.element.value !== "") {
+      this.element.classList.add("has-value");
+    }
+  }
+
   toggleStateClass() {
     this.element.classList.toggle("has-value", this.element.value !== "")
   }
